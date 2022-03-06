98e2fc1854b753836b2b20154c3d596fe5738f97
Zhansaule Telisheva <jansaule97@mail.ru>
2022-03-07 01:07:38 +0500

    calc module is created

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import modal from './modules/modal';
 import validation from './modules/validation';
 import tabs from './modules/tabs';
 import slider from './modules/slider';
+import calc from './modules/calc';
 
 
 timer('1 March 2022');
@@ -12,4 +13,5 @@ modal();
 validation();
 tabs();
 slider();
+calc(100);
 
