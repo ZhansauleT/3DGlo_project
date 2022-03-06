c89c4044e360b797515b0e4c05ee66724b07ccd9
Zhansaule Telisheva <jansaule97@mail.ru>
2022-03-07 00:40:19 +0500

    slider module is created

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import menu from './modules/menu';
 import modal from './modules/modal';
 import validation from './modules/validation';
 import tabs from './modules/tabs';
+import slider from './modules/slider';
 
 
 timer('1 March 2022');
@@ -10,4 +11,5 @@ menu();
 modal();
 validation();
 tabs();
+slider();
 
